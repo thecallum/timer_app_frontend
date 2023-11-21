3e5757067003f9acedd4adbbaab2a50e85b1bc62
thecallum <callummac@protonmail.com>
2023-11-21 09:26:59 +0000

    Persist timer to localStorage

--- src/features/timer/hooks/useTimer.ts
+++ src/features/timer/hooks/useTimer.ts
@@ -1,9 +1,23 @@
 import { useState, useEffect, useRef } from "react";
 
+const LOCAL_STORAGE_KEY = "startTime";
+
 export const useTimer = () => {
   const interval = useRef<number | undefined>();
   const [isRunning, setIsRunning] = useState(false);
-  const [time, setTime] = useState(0);
+  const [time, setTime] = useState<number>(0);
+
+  useEffect(() => {
+    // initial load
+    const savedStartTime = window.localStorage.getItem(LOCAL_STORAGE_KEY);
+
+    if (savedStartTime !== null) {
+      const time = getElapsedSeconds(savedStartTime);
+      console.log({ time });
+      setTime(time);
+      startTimer();
+    }
+  }, []);
 
   useEffect(() => {
     if (!isRunning) {
@@ -18,12 +32,31 @@ export const useTimer = () => {
     return () => clearInterval(interval.current);
   }, [isRunning, time]);
 
+  const getElapsedSeconds = (timestampString: string) => {
+    const timestamp = new Date(parseInt(timestampString));
+
+    const now = new Date();
+    const differenceInMilliseconds = now.getTime() - timestamp.getTime();
+    const differenceInSeconds = Math.floor(differenceInMilliseconds / 1000);
+
+    return differenceInSeconds;
+  };
+
   const startTimer = () => {
     setIsRunning(true);
   };
 
+  const handleStartTimer = () => {
+    startTimer();
+
+    // record timer started
+    const timestamp = Date.now().toString();
+    window.localStorage.setItem(LOCAL_STORAGE_KEY, timestamp);
+  };
+
   const stopTimer = () => {
     setIsRunning(false);
+    window.localStorage.removeItem(LOCAL_STORAGE_KEY);
     reset();
   };
 
@@ -32,7 +65,7 @@ export const useTimer = () => {
   };
 
   return {
-    startTimer,
+    startTimer: handleStartTimer,
     stopTimer,
     time,
     isRunning,
