6ae7c78af389ae4c40a60435af0a7c3eb98aa344
thecallum <callummac@protonmail.com>
2023-12-04 09:04:56 +0000

    Update placeholder events dates

--- src/contexts/calendarEventContext/placeholderEvents.ts
+++ src/contexts/calendarEventContext/placeholderEvents.ts
@@ -7,22 +7,22 @@ export const placeholderEvents: ICalendarEvent[] = [
   {
     id: uuidv4(),
     description: "Planning session",
-    start: dayjs("Tue Nov 07 2023 01:15:53 GMT+0000"),
-    end: dayjs("Tue Nov 07 2023 03:15:00 GMT+0000"),
+    start: dayjs("Tue Dec 04 2023 01:15:53 GMT+0000"),
+    end: dayjs("Tue Dec 04 2023 03:15:00 GMT+0000"),
     project: placeholderProjects[0],
   },
   {
     id: uuidv4(),
     description: "Standup meeting",
-    start: dayjs("Thu Nov 09 2023 03:30:53 GMT+0000"),
-    end: dayjs("Thu Nov 09 2023 05:02:28 GMT+0000"),
+    start: dayjs("Thu Dec 06 2023 03:30:53 GMT+0000"),
+    end: dayjs("Thu Dec 06 2023 05:02:28 GMT+0000"),
     project: placeholderProjects[1],
   },
   {
     id: uuidv4(),
     description: "Planning session",
-    start: dayjs("Thu Nov 09 2023 07:12:06 GMT+0000"),
-    end: dayjs("Thu Nov 09 2023 09:04:28 GMT+0000"),
+    start: dayjs("Thu Dec 06 2023 07:12:06 GMT+0000"),
+    end: dayjs("Thu Dec 06 2023 09:04:28 GMT+0000"),
     project: placeholderProjects[2],
   },
 ];
