87108fd8e6eadf55bc49bfcd95c46fd53c86cf56
thecallum <callummac@protonmail.com>
2024-01-29 19:35:39 +0000

    shrink build

--- next.config.js
+++ next.config.js
@@ -9,7 +9,13 @@ const nextConfig = {
     },
   ],
   distDir: "build/_next",
-  target: "server"
+  target: "server",
+  swcMinify: true,
+  images: {
+    // Nowhere to cache the images in Lambda (read only)
+    unoptimized: true, // Next 12.3+, other "experimental -> images -> unoptimized"
+  },
+  output: "standalone", // THIS IS IMPORTANT
 }
 
 module.exports = nextConfig
