3a2bcdd35da632517e861b38e05d624b8e281ece
thecallum <callummac@protonmail.com>
2024-01-30 09:20:56 +0000

    Configure domain

--- sst.config.ts
+++ sst.config.ts
@@ -10,7 +10,12 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, 'site')
+      const site = new NextjsSite(stack, 'site', {
+        customDomain:
+          stack.stage === 'production'
+            ? 'timer-app.thecallum.com'
+            : 'timer-app-development.thecallum.com',
+      })
 
       stack.addOutputs({
         SiteUrl: site.url,
