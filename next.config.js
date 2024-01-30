066ac8c7ca2ac18f17b10999706a00f296918ba6
thecallum <callummac@protonmail.com>
2024-01-30 19:36:14 +0000

    attempt 2

--- next.config.js
+++ next.config.js
@@ -13,6 +13,10 @@ const nextConfig = {
     unoptimized: true,
   },
   output: 'standalone',
+  env: {
+    API_KEY: process.env.SERVICE_API_KEY ?? '',
+    API_URL: process.env.SERVICE_API_URL ?? '',
+  },
 }
 
 module.exports = nextConfig
