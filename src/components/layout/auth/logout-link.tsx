dfbbc7c7ad3bc0da4601915db898d73a4c8effd8
thecallum <callummac@protonmail.com>
2024-02-16 16:22:59 +0000

    Simplify logout route

--- src/components/layout/auth/logout-link.tsx
+++ src/components/layout/auth/logout-link.tsx
@@ -3,7 +3,8 @@ const clientId = process.env.NEXT_PUBLIC_CLIENT_ID
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
 
 export const LogoutLink = () => {
-  const logoutUrl = `${domain}/v2/logout?client_id=${clientId}&returnTo=${baseUrl}`
+  // const logoutUrl = `${domain}/v2/logout?client_id=${clientId}&returnTo=${baseUrl}`
+  const logoutUrl = `/api/logout`
 
   return <a href={logoutUrl}>Sign out</a>
 }
