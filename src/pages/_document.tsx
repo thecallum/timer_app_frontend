f6ee0956c9ba1f651b062c92d13111d007bd74f2
thecallum <callummac@protonmail.com>
2024-03-01 18:42:18 +0000

    Try avoid font shift

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,20 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head></Head>
+      <Head>
+        <>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin={''}
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Nova+Square&family=Orbitron&display=swap"
+            rel="stylesheet"
+          />
+        </>
+      </Head>
       <body className="bg-slate-50 box-border">
         <Main />
         <NextScript />
