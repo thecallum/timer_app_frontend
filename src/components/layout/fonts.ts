2a4ea42cbf5a17783235a134cfb6c4094979b01b
thecallum <callummac@protonmail.com>
2024-03-01 19:55:48 +0000

    LF

--- src/components/layout/fonts.ts
+++ src/components/layout/fonts.ts
@@ -1,14 +1,13 @@
-import { Inter, Orbitron } from "next/font/google"
+import { Inter, Orbitron } from 'next/font/google'
 
 export const inter = Inter({
-    subsets: ['latin'],
-    variable: '--font-inter',
-    display: 'swap',
-  })
-  
-  export const orbitron = Orbitron({
-    subsets: ['latin'],
-    variable: '--font-orbitron',
-    display: 'swap',
-  })
-  
+  subsets: ['latin'],
+  variable: '--font-inter',
+  display: 'swap',
+})
+
+export const orbitron = Orbitron({
+  subsets: ['latin'],
+  variable: '--font-orbitron',
+  display: 'swap',
+})
--- src/components/layout/logo.tsx
+++ src/components/layout/logo.tsx
@@ -1,15 +1,15 @@
-import classNames from "classnames"
-import { orbitron } from "./fonts"
+import classNames from 'classnames'
+import { orbitron } from './fonts'
 
 export const Logo = () => {
   return (
     <div className="justify-center items-center flex w-full h-14">
-      <div className={
-        classNames([
+      <div
+        className={classNames([
           orbitron.className,
-          "bg-slate-300 rounded-md py-1 px-2 h-8 text-slate-500 shadow-xl"
-        ])
-      }>
+          'bg-slate-300 rounded-md py-1 px-2 h-8 text-slate-500 shadow-xl',
+        ])}
+      >
         Time Tracker
       </div>
     </div>
