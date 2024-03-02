3171ca140e7315c8c454680df5f0e4812a9d1ee4
thecallum <callummac@protonmail.com>
2024-03-02 09:22:02 +0000

    Resolve font issue - I think

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,23 +1,12 @@
+import { inter } from '@/components/layout/fonts'
+import classNames from 'classnames'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin={''}
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Nova+Square&family=Orbitron&display=swap"
-            rel="stylesheet"
-          />
-        </>
-      </Head>
-      <body className="bg-slate-50 box-border">
+      <Head></Head>
+      <body className={classNames([inter.className, 'bg-slate-50 box-border'])}>
         <Main />
         <NextScript />
       </body>
