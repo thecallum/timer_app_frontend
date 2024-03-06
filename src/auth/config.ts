d408430e981bfce2c36d678183fabcbb8feae998
thecallum <callummac@protonmail.com>
2024-03-06 07:54:47 +0000

    remove redundant public envs

--- src/auth/config.ts
+++ src/auth/config.ts
@@ -1,18 +1,15 @@
 import { Config } from 'sst/node/config'
 
-export const AUTH_DOMAIN = process.env.NEXT_PUBLIC_AUTH_DOMAIN
-export const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID
+export const AUTH_DOMAIN = process.env.AUTH_DOMAIN
+export const CLIENT_ID = process.env.CLIENT_ID
+export const AUTH_AUDIENCE = process.env.AUDIENCE
+export const CLIENT_SECRET = Config.CLIENT_SECRET
+export const AUTH_SCOPE = 'openid profile email offline_access'
 
 export const REDIRECT_URI =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000/api/authorize'
-    : process.env.NEXT_PUBLIC_REDIRECT_URI
-
-export const CLIENT_SECRET = Config.CLIENT_SECRET
-
-export const AUTH_AUDIENCE = process.env.NEXT_PUBLIC_AUDIENCE
-
-export const AUTH_SCOPE = 'openid profile email offline_access'
+    : process.env.REDIRECT_URI
 
 export const COOKIE_DOMAIN =
   process.env.NODE_ENV === 'development'
