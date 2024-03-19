132b0bd044329092e6a7dd92e055b5f7d1d8ebf5
Callum Macpherson <callummac@protonmail.com>
2024-03-19 16:36:17 +0000

    Remove effect from createProjectMdoal

--- src/contexts/createProjectModalContext/hooks/useProjectModal.ts
+++ src/contexts/createProjectModalContext/hooks/useProjectModal.ts
@@ -1,6 +1,8 @@
 import { useClickOutContext } from '@/contexts/clickOutContext'
 import { useProjectsContext } from '@/contexts/projectsContext'
 import { ProjectApiRequestObject } from '@/requests/types'
+import { Color, ProjectColors } from '@/types/colors'
+import { ProjectColor, defaultProjectColor } from '@/types/projects'
 import { useState } from 'react'
 
 export const useProjectModal = () => {
@@ -8,15 +10,31 @@ export const useProjectModal = () => {
   const { setModalAsOpen } = useClickOutContext()
 
   const [modalIsOpen, setModalIsOpen] = useState(false)
+  const [modalColor, setModalColor] =
+    useState<ProjectColor>(defaultProjectColor)
+
   const [requestError, setRequestError] = useState<string | null>(null)
   const [isLoading, setIsLoading] = useState(false)
 
   const closeModal = () => {
     setTimeout(() => setModalIsOpen(false))
+
     setModalAsOpen(false)
   }
 
+  const getRandomProjectColor = (): ProjectColor => {
+    const enumValues: Color[] = Object.values(Color).filter(
+      (value) => typeof value === 'number',
+    ) as Color[]
+
+    const randomIndex = Math.floor(Math.random() * enumValues.length) // Generate a random index
+    const randomEnumKey = enumValues[randomIndex] // Get the enum key
+
+    return ProjectColors[randomEnumKey]
+  }
+
   const openModal = () => {
+    setModalColor(() => getRandomProjectColor())
     setModalIsOpen(true)
     setModalAsOpen(true)
   }
@@ -46,5 +64,6 @@ export const useProjectModal = () => {
     closeModal,
     openModal,
     onCreateProject,
+    modalColor,
   }
 }
