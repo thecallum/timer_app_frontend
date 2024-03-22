79b58b98271d9e836cc30f91d63f162adea27a8a
Callum Macpherson <callummac@protonmail.com>
2024-03-22 15:46:40 +0000

    various improvements

--- src/features/calendar/components/calendar-dates.tsx
+++ src/features/calendar/components/calendar-dates.tsx
@@ -23,14 +23,14 @@ export const CalendarDates = () => {
   })
 
   return (
-    <div className="ml-6 mb-2 h-12 lg:ml-16">
+    <div className="ml-6 mb-2 h-12 lg:ml-16 mr-[10px]">
       <ul className="flex justify-between" aria-label="Days of week">
         {weekDaysArray.map(({ day, name, time, current, date }) => (
-          <li className="flex-grow" key={name}>
-            <div className="flex flex-col items-center lg:flex-row justify-center">
+          <li className="flex-grow flex-shrink-0" key={name}>
+            <div className="flex flex-col items-center xl:flex-row justify-center">
               <div
                 className={classNames(
-                  ' text-2xl font-light w-9 h-9 rounded-full flex items-center justify-center lg:mr-3',
+                  ' text-2xl font-light w-9 h-9 rounded-full flex items-center justify-center xl:mr-3',
                   {
                     'bg-purple-100': current,
                     'text-slate-600': !current,
@@ -53,7 +53,7 @@ export const CalendarDates = () => {
                   {name}
                 </div>
                 <div
-                  className="text-slate-500 text-xs hidden lg:block"
+                  className="text-slate-500 text-xs hidden xl:block"
                   aria-label={`Total event duration for ${dateFormat(date, 'dddd')}`}
                 >
                   {time}
