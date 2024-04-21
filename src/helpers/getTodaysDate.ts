394ce3338a8743602cdfb35d069443e4208b2eb7
Callum Macpherson <callummac@protonmail.com>
2024-04-21 19:12:20 +0100

    sdfsdf

--- src/helpers/getTodaysDate.ts
+++ src/helpers/getTodaysDate.ts
@@ -2,7 +2,7 @@ export const getTodaysDate = () => {
   const today = new Date()
 
   //eslint-disable-next-line no-extra-boolean-cast
-  const useMockDate: boolean = process.env.NEXT_TEST_ENV === "true"
+  const useMockDate: boolean = process.env.NEXT_TEST_ENV === 'true'
 
   // console.log({ NEXT_TEST_ENV: process.env.NEXT_TEST_ENV })
 
