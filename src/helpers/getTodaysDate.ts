7884d84f99b37c7bbc0cfe4867a2dd6cda0fe828
Callum Macpherson <callummac@protonmail.com>
2024-04-21 13:56:39 +0100

    should actually work

--- src/helpers/getTodaysDate.ts
+++ src/helpers/getTodaysDate.ts
@@ -2,7 +2,9 @@ export const getTodaysDate = () => {
   const today = new Date()
 
   //eslint-disable-next-line no-extra-boolean-cast
-  if (!!process.env.TEST_ENV) {
+  const useMockDate: boolean = !!process.env.TEST_ENV
+
+  if (useMockDate) {
     // Parse the environment variable to a Date object
     const hardCodedDate = new Date(
       process.env.NEXT_PUBLIC_TODAYS_DATE as string,
