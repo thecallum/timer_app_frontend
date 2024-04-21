47da1d48578391f04664c31fd868a109d394c458
Callum Macpherson <callummac@protonmail.com>
2024-04-21 19:51:17 +0100

    REEEEEEEEEEEEEEEEEEEEEEE

--- src/helpers/getTodaysDate.ts
+++ src/helpers/getTodaysDate.ts
@@ -2,9 +2,12 @@ export const getTodaysDate = () => {
   const today = new Date()
 
   //eslint-disable-next-line no-extra-boolean-cast
-  const useMockDate: boolean = process.env.NEXT_TEST_ENV === 'true'
+  const useMockDate: boolean = process.env.NEXT_PUBLIC_TEST_ENV === 'true'
 
-  console.log({ NEXT_TEST_ENV: process.env.NEXT_TEST_ENV, useMockDate })
+  console.log({
+    NEXT_PUBLIC_TEST_ENV: process.env.NEXT_PUBLIC_TEST_ENV,
+    useMockDate,
+  })
 
   if (useMockDate) {
     // Parse the environment variable to a Date object
