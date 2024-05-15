15421f1eb060efb6d857a628a0070380089e3609
Callum Macpherson <callummac@protonmail.com>
2024-05-15 09:19:15 +0100

    Fix bug for multiple events

--- src/features/calendar/components/calendar-event.tsx
+++ src/features/calendar/components/calendar-event.tsx
@@ -1,11 +1,5 @@
-import { useProjectsContext } from '@/contexts/projectsContext'
-import { CalendarEventView } from './calendar-event-view'
-import { EditEventPopover } from './popovers/edit-event-popover'
 import { CalendarEvent as CalendarEventType } from '@/types/calendarEvents'
-import { defaultProjectColor } from '@/types/projects'
-import dateFormat from 'dateformat'
-import { usePopover } from '../hooks/usePopover'
-import { PopoverComponentWrapper } from './PopoverComponentWrapper'
+import { CalendarEventDisplayPosition } from './CalendarEventDisplayPosition'
 
 interface Props {
   event: CalendarEventType
@@ -15,80 +9,18 @@ interface Props {
 
 export const CalendarEvent = (props: Props) => {
   const { event, containerRef, gridSizeMultiplier } = props
-  const { getProjectById } = useProjectsContext()
-
-  const {
-    handleOpen,
-    handleClose,
-    setReferenceElement,
-    setPopperElement,
-    showPopover,
-    popperStyles,
-    popperAttributes,
-  } = usePopover(containerRef)
-
-  const {
-    description,
-    projectId,
-    durationInSeconds,
-    displayPositions,
-    startTime,
-  } = event
-
-  const project = getProjectById(projectId)
-  const projectColor = project?.projectColor ?? defaultProjectColor
 
   return (
     <>
-      {displayPositions.map((x, index) => {
-        const { height, top, left, width, column } = x
-
-        const eventStyles = {
-          height: `${(height / 2) * gridSizeMultiplier}px`,
-          top: `${(top / 2) * gridSizeMultiplier}px`,
-          left: `calc((100% / 7 * ${column}) + (100% / 7 * ${left}))`,
-          width: `calc((100%/7)*${width})`,
-        }
-
-        return (
-          <li key={index} className="relative">
-            <PopoverComponentWrapper
-              showPopover={showPopover}
-              setRef={setPopperElement}
-              popperStyles={popperStyles}
-              popperAttributes={popperAttributes}
-            >
-              <EditEventPopover
-                containerRef={containerRef}
-                event={event}
-                close={handleClose}
-              />
-            </PopoverComponentWrapper>
-
-            <div
-              style={eventStyles}
-              className="absolute p-[1px] overflow-hidden"
-              role="article"
-              aria-label={`Calendar event: ${description} on ${dateFormat(startTime, 'mmmm dd')} at ${dateFormat(startTime, 'h:MM TT')}, ${project?.description ? `assigned to project ${project?.description}` : `not assigned to any project`}.`}
-            >
-              <button
-                className={`w-full h-full cursor-pointer rounded-sm`}
-                ref={setReferenceElement}
-                onClick={handleOpen}
-                style={{
-                  background: projectColor.light,
-                }}
-              >
-                <CalendarEventView
-                  description={description}
-                  durationInSeconds={durationInSeconds}
-                  project={project}
-                />
-              </button>
-            </div>
-          </li>
-        )
-      })}
+      {event.displayPositions.map((x, index) => (
+        <CalendarEventDisplayPosition
+          displayPosition={x}
+          event={event}
+          key={index}
+          containerRef={containerRef}
+          gridSizeMultiplier={gridSizeMultiplier}
+        />
+      ))}
     </>
   )
 }
