afbf53eef82f890b747d0c0b27bc33a712b26abb
Callum Macpherson <callummac@protonmail.com>
2024-06-03 12:27:30 +0100

    refactor calcualte timeslots

--- src/contexts/calendarEventContext/hooks/useCalendar/calculateEventDisplayPositions/getEventsByTimeslot.ts
+++ src/contexts/calendarEventContext/hooks/useCalendar/calculateEventDisplayPositions/getEventsByTimeslot.ts
@@ -8,33 +8,16 @@ export const getEventsByTimeslot = (
   events: CalendarEvent[],
   dayOfWeek: Date,
 ) => {
-  // let's try using an array immediatly instead of an object
-  const timeslotsByTimestamp: string[][] = []
-
-  // 1. group events into 5 minute timeslots
-  // eg [05:10]: [event1, event2]
-
-  let firstFiveMinuteSlot = 0
-  let lastFiveMinuteSlot = 0
+  const timeslotsByTimestamp: string[][] = Array.from(
+    { length: BOTTOM_SLOT + 1 },
+    () => [],
+  )
 
   events.forEach((event) => {
-    if (isSameDay(event.startTime, event.endTime)) {
-      // is single column
-      firstFiveMinuteSlot = Math.floor(event.startTimeInSeconds / 60 / 5) - 1
-      lastFiveMinuteSlot = Math.ceil(event.endTimeInSeconds / 60 / 5) - 1
-    } else if (isSameDay(event.startTime, dayOfWeek)) {
-      // multiple, but first column
-      firstFiveMinuteSlot = Math.floor(event.startTimeInSeconds / 60 / 5) - 1
-      lastFiveMinuteSlot = BOTTOM_SLOT
-    } else if (isSameDay(event.endTime, dayOfWeek)) {
-      // is last column, but not single column
-      firstFiveMinuteSlot = TOP_SLOT
-      lastFiveMinuteSlot = Math.ceil(event.endTimeInSeconds / 60 / 5) - 1
-    } else {
-      // full height
-      firstFiveMinuteSlot = TOP_SLOT
-      lastFiveMinuteSlot = BOTTOM_SLOT
-    }
+    const { firstFiveMinuteSlot, lastFiveMinuteSlot } = calculateSlots(
+      event,
+      dayOfWeek,
+    )
 
     // loop over each of the 5 minute slots
 
@@ -43,14 +26,45 @@ export const getEventsByTimeslot = (
       i <= Math.min(lastFiveMinuteSlot, BOTTOM_SLOT);
       i++
     ) {
-      if (Object.prototype.hasOwnProperty.call(timeslotsByTimestamp, i)) {
-        timeslotsByTimestamp[i].push(event.id)
-      } else {
-        timeslotsByTimestamp[i] = [event.id]
-      }
+      timeslotsByTimestamp[i].push(event.id)
     }
   })
 
   // convert to set for easier access
   return timeslotsByTimestamp.map((x) => new Set<string>(x))
+}
+
+const calculateSlots = (event: CalendarEvent, dayOfWeek: Date) => {
+  // 1. group events into 5 minute timeslots
+  // eg [05:10]: [event1, event2]
+
+  if (isSameDay(event.startTime, event.endTime)) {
+    // is single column
+    const firstFiveMinuteSlot = Math.floor(event.startTimeInSeconds / 60 / 5)
+    const lastFiveMinuteSlot = Math.ceil(event.endTimeInSeconds / 60 / 5)
+
+    return { firstFiveMinuteSlot, lastFiveMinuteSlot }
+  }
+
+  if (isSameDay(event.startTime, dayOfWeek)) {
+    // multiple, but first column
+    const firstFiveMinuteSlot = Math.floor(event.startTimeInSeconds / 60 / 5)
+    const lastFiveMinuteSlot = BOTTOM_SLOT
+
+    return { firstFiveMinuteSlot, lastFiveMinuteSlot }
+  }
+
+  if (isSameDay(event.endTime, dayOfWeek)) {
+    // is last column, but not single column
+    const firstFiveMinuteSlot = TOP_SLOT
+    const lastFiveMinuteSlot = Math.ceil(event.endTimeInSeconds / 60 / 5)
+
+    return { firstFiveMinuteSlot, lastFiveMinuteSlot }
+  }
+
+  // full height
+  const firstFiveMinuteSlot = TOP_SLOT
+  const lastFiveMinuteSlot = BOTTOM_SLOT
+
+  return { firstFiveMinuteSlot, lastFiveMinuteSlot }
 }
